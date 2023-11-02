9e4722f5c716b2b1cb295de13e74ca685b832158
Erin Wolff <erin_wolff@outlook.com>
2023-11-02 09:32:59 -0700

    added error handler to server.js & error handling in authors route

--- api/authors.js
+++ api/authors.js
@@ -4,15 +4,19 @@ const prisma = require('../prisma')
 module.exports = router;
 
 // /api/authors returns an array of all authors
-router.get('/', async (req, res) => {
-  const authors = await prisma.author.findMany();
-  res.send(authors);
+router.get('/', async (req, res, next) => {
+  try {
+    const authors = await prisma.author.findMany();
+    res.send(authors);
+  } catch {
+    next();
+  }
 });
 
 
 
 // /api/authors/:id returns a single author with the specified id
-router.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res, next) => {
   const id = +req.params.id
   const result = await prisma.author.findUnique({
     where: {
@@ -23,7 +27,7 @@ router.get('/:id', async (req, res) => {
 })
 
 // /api/authors/:id/books get all books written by the specified author
-router.get('/:id/books', async (req, res) => {
+router.get('/:id/books', async (req, res, next) => {
   const id = +req.params.id
   const result = await prisma.author.findUnique({
     where: {
@@ -37,74 +41,130 @@ router.get('/:id/books', async (req, res) => {
 })
 
 // /api/authors creates a new author with the information provided in the request body
-router.post('/', async (req, res) => {
-  const { name, book1, book2, book3 } = req.body
-  console.log(req.body)
-  const author = await prisma.author.create({
-    data: {
-      name: name,
-      books: {
-        create: [
-          { title: book1 },
-          { title: book2 },
-          { title: book3 },
-        ],
+router.post('/', async (req, res, next) => {
+  try {
+    const { name, book1, book2, book3 } = req.body
+    if (!name) {
+      const error = {
+        status: 400,
+        message: 'Author must have a name.',
+      };
+      return next(error);
+    }
+    const author = await prisma.author.create({
+      data: {
+        name: name,
+        books: {
+          create: [
+            { title: book1 },
+            { title: book2 },
+            { title: book3 },
+          ],
+        },
       },
-    },
-  })
-  res.send(author);
-})
+    })
+    res.send(author);
+  } catch {
+    next();
+  }
+});
 
 // /api/authors/:id overwrites the author with the information provided in the request body
-router.put('/:id', async (req, res) => {
-  const id = +req.params.id
-  const { name, book1, book2, book3 } = req.body
-  const author = await prisma.author.update({
-    where: { id: id },
-    data: {
-      name: name,
-      books: {
-        create: [
-          { title: book1 },
-          { title: book2 },
-          { title: book3 },
-        ],
+router.put('/:id', async (req, res, next) => {
+  try {
+    const id = +req.params.id;
+
+    const authorExists = await prisma.author.findUnique({ where: { id } });
+    if (!authorExists) {
+      return next({
+        status: 404,
+        message: `Could not find author with id ${id}.`,
+      });
+    }
+    const { name, book1, book2, book3 } = req.body
+    if (!name) {
+      return next({
+        status: 400,
+        message: 'Author must have a name'
+      });
+    }
+    const author = await prisma.author.update({
+      where: { id: id },
+      data: {
+        name: name,
+        books: {
+          create: [
+            { title: book1 },
+            { title: book2 },
+            { title: book3 },
+          ],
+        },
       },
-    },
-  })
-  res.send(author);
+    })
+    res.send(author);
+  } catch {
+    next();
+  }
 })
 
 
 // /api/authors/:id deletes the author with the specified id
-router.delete('/:id', async (req, res) => {
-  const id = +req.params.id
-  const author = await prisma.author.delete({
-    where: { id: id },
-  })
-  res.send(author);
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const id = +req.params.id
+    const authorExists = await prisma.author.findUnique({ where: { id } });
+    if (!authorExists) {
+      return next({
+        status: 404,
+        message: `Could not find author with id ${id}`,
+      });
+    }
+    const author = await prisma.author.delete({
+      where: { id: id },
+    })
+    res.sendStatus(204);
+  } catch {
+    next();
+  }
 })
 
 // /api/author/:id/books creates a new book as provided in the request body with the specified author
-router.post('/:id/books', async (req, res) => {
-  const id = +req.params.id
-  const { newbook } = req.body
-  const result = await prisma.author.update({
-    where: {
-      id: id,
-    },
-    include: {
-      books: true,
-    },
-    data: {
-      books: {
-        create: [
-          { title: newbook },
-        ],
+router.post('/:id/books', async (req, res, next) => {
+  try {
+    const id = +req.params.id
+    const author = await prisma.author.findUnique({ where: { id } });
+    if (!author) {
+      return next({
+        staus: 404,
+        message: `Could not find author with ${id}`,
+      });
+    }
+    const { newbook } = req.body
+    if (!newbook) {
+      return next({
+        status: 400,
+        message: 'Book must have a title',
+      });
+    }
+    const result = await prisma.author.update({
+      where: {
+        id: id,
       },
-    },
-  })
-  res.send(result)
+      include: {
+        books: true,
+      },
+      data: {
+        books: {
+          create: [
+            { title: newbook },
+          ],
+        },
+      },
+    })
+    res.send(result);
+  } catch {
+    next();
+  }
 })
 
 
--- server.js
+++ server.js
@@ -14,6 +14,14 @@ app.use('/api/books', require('./api/books.js'));
 
 const PORT = 3000
 
+// Simple error handling middleware
+app.use((err, req, res, next)=> {
+  console.error(err);
+  const status = err.status ?? 500;
+  const message = err.message ?? 'Internal server error.';
+  res.status(status).json({message});
+});
+
 app.listen(PORT, () => {
   console.log('Listening! URL: http://localhost:' + PORT);
 });
